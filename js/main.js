887f647dd7c5d00a54bcfdf5ff7022376e4d50b5
papgergo1997 <pap.gergo.1997@gmail.com>
2021-01-10 22:02:03 +0100

    bootstrap-project-desktop-design-90%-completed

--- js/main.js
+++ js/main.js
@@ -2,4 +2,17 @@ $('[data-toggle="tooltip"]').tooltip();
 
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
-});  
+});
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
